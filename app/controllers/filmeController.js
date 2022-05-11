258ec14401d1e620f1475ef7cd49ea37693c2c8e
Cássio Fehlberg Lemos <cassiofehkberg@gmail.com>
2022-05-11 18:20:20 -0300

    update

---
+++ app/controllers/filmeController.js
@@ -0,0 +1,54 @@
+const models = require('../models/models');
+
+module.exports = {
+    getFilme: async (req, res) => {
+        try {
+            const retorno = await models.getFilme(); 
+    
+            return res.json(retorno);
+        } catch (err) {
+            return res.json({ error: err });
+        }
+    },
+
+    getFilmeId: async (req, res) => {
+        try {
+            const retorno = await models.getFilmeId(req.params.id); 
+    
+            return res.json(retorno);
+        } catch (err) {
+            return res.json({ error: err });
+        }
+    },
+
+    createFilme: async (req, res) => {
+        try {
+            const retorno = await models.createFilme(req.body); 
+    
+            return res.json(retorno);
+        } catch (err) {
+            return res.json({ error: err });
+        }
+    },
+    
+    updateFilme: async (req, res) => {
+        try {
+            const retorno = await models.updateFilme(req.params.id, req.body); 
+    
+            return res.json(retorno);
+        } catch (err) {
+            return res.json({ error: err });
+        }
+    },
+
+    deleteFilme: async (req, res) => {
+        try {
+            const retorno = await models.deleteFilme(); 
+    
+            return res.json(retorno);
+        } catch (err) {
+            return res.json({ error: err });
+        }
+    },
+}
+  retorno
---
+++ app/models/filmeModel.js
@@ -0,0 +1,58 @@
+const database = require('./database');
+
+module.exports = {
+    getFilme: async () => {
+        return new Promise((resolve, reject) => {
+            const query = `SELECT * FROM filme;`
+    
+            database.query(query, (err, res) => {
+                if (res) resolve(res);
+                else reject(err)
+            });
+        });
+    },
+
+    getFilmeId: async (id) => {
+        return new Promise((resolve, reject) => {
+            const query = `SELECT * FROM filme WHERE id=${id};`
+    
+            database.query(query, (err, res) => {
+                if (res) resolve(res);
+                else reject(err)
+            });
+        });
+    },
+
+    createFilme: async (filme) => {
+        return new Promise((resolve, reject) => {
+            const query = `INSERT INTO filme (titulo,imagem,descricao) values (${filme.titulo}, ${filme.imagem}, ${filme.descricao});`
+    
+            database.query(query, (err, res) => {
+                if (res) resolve(res);
+                else reject(err)
+            });
+        });
+    },
+
+    updateFilme: async (id, filme) => {
+        return new Promise((resolve, reject) => {
+            const query = `UPDATE filme SET titulo=${filme.titulo}, imagem=${filme.imagem}, descricao=${descricao} WHERE id=${id};`
+    
+            database.query(query, (err, res) => {
+                if (res) resolve(res);
+                else reject(err)
+            });
+        });
+    },
+
+    deleteFilme: async (id) => {
+        return new Promise((resolve, reject) => {
+            const query = `DELETE FROM filme WHERE id=${id};`
+    
+            database.query(query, (err, res) => {
+                if (res) resolve(res);
+                else reject(err)
+            });
+        });
+    },
+}
