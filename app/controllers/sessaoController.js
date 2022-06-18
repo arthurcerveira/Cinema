a97a90bc9f9aa8b3094c5c6d7e770f8abc573437
Gabriel Gomes <gabriel.almgom@gmail.com>
2022-06-18 10:39:15 -0300

    remove/9

--- app/controllers/sessaoController.js
+++ app/controllers/sessaoController.js
@@ -72,7 +72,7 @@ module.exports = {
             const hojeNovo = new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate(), hoje.getHours() - 3, 59, 58)
             const hojeFormatado = hojeNovo.toISOString().slice(0, 19).replace('T', ' ').replaceAll('/', '-')
 
-9
+
             const nextWeek = new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate() + 7, 20, 59, 58)
             const nextWeekFormatado = nextWeek.toISOString().slice(0, 19).replace('T', ' ').replaceAll('/', '-')
 
