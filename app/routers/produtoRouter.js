416c66e9f34c151e3f8f7295fce7864151932a2a
Gabriel Gomes <gabriel.almgom@gmail.com>
2022-06-18 10:45:56 -0300

    feat/getIngresso

--- app/routers/produtoRouter.js
+++ app/routers/produtoRouter.js
@@ -5,10 +5,12 @@ const produtoController = require('../controllers/produtoController.js')
 const produtoRouter = express.Router()
 
 produtoRouter.get('/produto/', produtoController.getProduto)
+produtoRouter.get('/produto/ingresso', produtoController.getIngresso)
 produtoRouter.get('/produto/:id', produtoController.getProdutoId)
 produtoRouter.put('/produto/:id', validateAdminToken, produtoController.updateProduto)
 produtoRouter.post('/produto/', validateAdminToken, produtoController.createProduto)
 produtoRouter.delete('/produto/:id', validateAdminToken, produtoController.deleteProduto)
+
 
 
 
